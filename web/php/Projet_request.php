4efbdee213993f0295d0d277d00e3597610e9754
Raptoragile <80196313+Raptoragile@users.noreply.github.com>
2021-06-21 14:33:42 +0200

    Update Projet_request.php

--- web/php/Projet_request.php
+++ web/php/Projet_request.php
@@ -21,63 +21,41 @@
   $data = false;
 
   //request 1 :
-
   function user()
-
-    //$au = fetch('');
-
+    //$au = fetch('INSERT INTO utilisateur VALUES (val_pseudo, val_nom, val_prenom, val_email, SHA1(val_mot_de_passe) , val_num_tel);');
     //echo json_encode($au);
-
   }
 
   //request 2 :
-
-  function user_det()
-
-    $aud = fetch('');
-
+  function search_travel(bool $depart)
+    $aud = fetch('SELECT * FROM trajet WHERE nombre_place_restante>0 AND debute_isen==$depart AND id_trajet<=10;');
     echo json_encode($aud);
-
   }
 
   //request 3 :
-
-  function materiel()
-
-    $am = fetch('');
-
+  function check_connect()
+    $am = fetch('SELECT * FROM utilisateur WHERE pseudonyme==(val_pseudo) AND mot_de_passe==SHA1(val_mot_de_passe);');
     echo json_encode($am);
-
   }
 
   //request 4 :
-
-  function materiel_det()
-
-    $amt = fetch('');
+  function add_travel(smallint $val_place, smallint $val_prix, varchar $adresse, datetime $date_debut, datetime $date_fin, bool $depart)
+    $amt = fetch('INSERT INTO trajet (nombre_place, nombre_place_restante, prix, adresse, date_depart, date_arrivee, debute_isen)
+VALUES ($val_place, $val_place, $val_prix, $adresse, $date_debut, $date_fin, $depart);');
 
     echo json_encode($amt);
-
   }
 
   //request 5 :
-
-  function aff1()
-
-    $aa1 = fetch('');
-
+  function search_travel_by_ID(int val_id)
+    $aa1 = fetch('SELECT * FROM trajet WHERE id_trajet==(val_id);');
     echo json_encode($aa1);
-
   }
 
   //request 6 :
-
-  function aff2()
-
-    $aa2 = fetch('');
-
+  function take_a_place()
+    $aa2 = fetch('UPDATE TABLE trajet SET nombre_place_restante -= 1 WHERE id_trajet==(val);');
     echo json_encode($aa2);
-
   }
 
     $phrase_request = 'SELECT prenom, nom FROM user';
