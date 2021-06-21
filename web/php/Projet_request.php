4c7623881c1e651f927388ab1e0950ec3a219617
Raptoragile <80196313+Raptoragile@users.noreply.github.com>
2021-06-21 14:59:31 +0200

    Update Projet_request.php

--- web/php/Projet_request.php
+++ web/php/Projet_request.php
@@ -20,25 +20,21 @@
         $id = NULL;
   $data = false;
 
-  //request 1 :
+  //request 1 : créer les utilisateurs
   function user()
     //$au = fetch('INSERT INTO utilisateur VALUES (val_pseudo, val_nom, val_prenom, val_email, SHA1(val_mot_de_passe) , val_num_tel);');
+    
     //echo json_encode($au);
   }
-
-  //request 2 :
-  function search_travel(bool $depart)
-    $aud = fetch('SELECT * FROM trajet WHERE nombre_place_restante>0 AND debute_isen==$depart AND id_trajet<=10;');
-    echo json_encode($aud);
-  }
-
-  //request 3 :
+  
+    //request 2 : vérifie les données d'un utilisateur
   function check_connect()
     $am = fetch('SELECT * FROM utilisateur WHERE pseudonyme==(val_pseudo) AND mot_de_passe==SHA1(val_mot_de_passe);');
+    
     echo json_encode($am);
   }
 
-  //request 4 :
+//request 3 : créer les trajets
   function add_travel(smallint $val_place, smallint $val_prix, varchar $adresse, datetime $date_debut, datetime $date_fin, bool $depart)
     $amt = fetch('INSERT INTO trajet (nombre_place, nombre_place_restante, prix, adresse, date_depart, date_arrivee, debute_isen)
 VALUES ($val_place, $val_place, $val_prix, $adresse, $date_debut, $date_fin, $depart);');
@@ -46,13 +42,20 @@ VALUES ($val_place, $val_place, $val_prix, $adresse, $date_debut, $date_fin, $de
     echo json_encode($amt);
   }
 
-  //request 5 :
+  //request 4 : recherche de trajets
+  function search_travel(bool $depart)
+    $aud = fetch('SELECT * FROM trajet WHERE nombre_place_restante>0 AND debute_isen==$depart AND id_trajet<=10;');
+    
+    echo json_encode($aud);
+  }
+
+  //request 5 : recherche un trajet particulier
   function search_travel_by_ID(int val_id)
     $aa1 = fetch('SELECT * FROM trajet WHERE id_trajet==(val_id);');
     echo json_encode($aa1);
   }
 
-  //request 6 :
+  //request 6 : reserve une place pour le trajet
   function take_a_place()
     $aa2 = fetch('UPDATE TABLE trajet SET nombre_place_restante -= 1 WHERE id_trajet==(val);');
     echo json_encode($aa2);
