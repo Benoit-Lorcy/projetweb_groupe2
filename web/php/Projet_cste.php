f55acfcb73adad5a1c89e5b157769072db0347f5
Raptoragile <80196313+Raptoragile@users.noreply.github.com>
2021-06-21 09:39:19 +0200

    Update Projet_cste.php

--- web/php/Projet_cste.php
+++ web/php/Projet_cste.php
@@ -2,7 +2,7 @@
 
   define('DB_USER', 'user2');
   define('DB_PASSWORD', 'Xujny6XPpptqucHi');
-  define('DB_NAME', 'database_HB_BL');
+  define('DB_NAME', 'user2');
   define('DB_SERVER', 'localhost');
 
 ?>
