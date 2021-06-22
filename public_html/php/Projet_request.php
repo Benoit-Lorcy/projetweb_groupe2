476b53de5c55a91628fc6d95e6759d13e214c3bf
Raptoragile <80196313+Raptoragile@users.noreply.github.com>
2021-06-22 11:32:51 +0200

    Update Projet_request.php

--- public_html/php/Projet_request.php
+++ public_html/php/Projet_request.php
@@ -21,21 +21,21 @@
   $data = false;
 
   //request 1 : créer les utilisateurs
-  function user(varchar $val_pseudo, varchar $val_nom, varchar $val_prenom, varchar $val_email, varchar $val_mot_de_passe, varchar $val_num_tel)
+  function user( varchar $val_pseudo, varchar $val_nom, varchar $val_prenom, varchar $val_email, varchar $val_mot_de_passe, varchar $val_num_tel )
     //$request_1 = fetch('INSERT INTO utilisateur VALUES ($val_pseudo, $val_nom, $val_prenom, $val_email, SHA1($val_mot_de_passe) , $val_num_tel);');
     
     //echo json_encode($request_1);
   }
   
     //request 2 : vérifie les données d'un utilisateur
-  function check_connect(varchar $val_pseudo, varchar $val_mot_de_passe)
+  function check_connect( varchar $val_pseudo, varchar $val_mot_de_passe )
     $request_2 = fetch('SELECT * FROM utilisateur WHERE pseudonyme==$val_pseudo AND mot_de_passe==SHA1($val_mot_de_passe);');
     
     echo json_encode($request_2);
   }
 
 //request 3 : créer les trajets
-  function add_travel(smallint $val_place, smallint $val_prix, varchar $adresse, datetime $date_debut, datetime $date_fin, bool $depart)
+  function add_travel( smallint $val_place, smallint $val_prix, varchar $adresse, datetime $date_debut, datetime $date_fin, bool $depart )
     $request_3 = fetch('INSERT INTO trajet (nombre_place, nombre_place_restante, prix, adresse, date_depart, date_arrivee, debute_isen)
 VALUES ($val_place, $val_place, $val_prix, $adresse, $date_debut, $date_fin, $depart);');
 
@@ -43,12 +43,12 @@ VALUES ($val_place, $val_place, $val_prix, $adresse, $date_debut, $date_fin, $de
   }
 
   //request 4 : recherche de trajets
-  function search_travel(bool $depart)
+  function search_travel( bool $depart )
   $ind=0;
   $ind_test=0;
   
   while( $ind<=10 ){
-    if( fetch(SELECT nombre_place_restante FROM trajet WHERE id_trajet==$ind_test)>0 ){
+    if( fetch(SELECT nombre_place_restante FROM trajet WHERE id_trajet==$ind_test) > 0 ){
       $request_4 = fetch('SELECT * FROM trajet WHERE AND debute_isen==$depart;');
       
       $ind++;
@@ -60,14 +60,14 @@ VALUES ($val_place, $val_place, $val_prix, $adresse, $date_debut, $date_fin, $de
   }
 
   //request 5 : recherche un trajet particulier
-  function search_travel_by_ID(int $val_id)
+  function search_travel_by_ID( int $val_id )
     $request_5 = fetch('SELECT * FROM trajet WHERE id_trajet==$val_id;');
     
     echo json_encode($request_5);
   }
 
   //request 6 : reserve une ou plusieurs places pour le trajet
-  function take_place(smallint $nb_place_prise, int $val_id)
+  function take_place( smallint $nb_place_prise, int $val_id )
     $request_6 = fetch('UPDATE TABLE trajet SET nombre_place_restante -= $nb_place_prise WHERE id_trajet==$val_id;');
     
     echo json_encode($request_6);
